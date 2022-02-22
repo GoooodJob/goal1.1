ae81ecb13754fb8e08ee9e918bf5a69ef009da5f
heran <1187228451@qq.com>
2022-02-22 14:26:06 +0800

    remove the useless codes

--- src/server.ts
+++ src/server.ts
@@ -5,14 +5,10 @@ const port: number = 3000;
 
 app.get('/', function (req, res) {res.end('Welcome Home!');})
 
-app.get('/helloworld', function (req, res) {
-    console.log('requested helloworld service!')
-    res.end('Hello World!')
-})
-
-// app.get('/helloworldx2', function (req, res) {
-//     res.end('Hello World!\nHello World!')
-//  })
+// app.get('/helloworld', function (req, res) {
+//     console.log('requested helloworld service!')
+//     res.end('Hello World!')
+// })
 
 app.listen(port, () => {
     console.log(`App listening on the http://localhost:${port}`)
