f5756131032db38624567c97518f0ba51e08d85b
heran <1187228451@qq.com>
2022-02-23 14:07:31 +0800

    将function改为箭头函数

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,9 @@ import * as express from 'express'
 const app = express();
 const port: number = 3000;
 
-app.get('/', function (req, res) {res.end('Welcome Home!');})
+app.get('/', (req, res) =>{res.end('Welcome Home!');})
 
-app.get('/helloworld', function (req, res) {
+app.get('/helloworld', (req, res) =>{
     console.log('requested helloworld service!')
     res.end('Hello World!')
 })
